0f3d3feb5928ef5ef8de9a8ec55f340dcd4e99e2
Michael <mikechansas@gmail.com>
2021-12-03 03:41:23 +0000

    ISS Promise Approach

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const nextISSTimesForMyLocation = require('./iss');
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const printPassTimes = require('./printTime');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  
   printPassTimes(passTimes);
 });
---
+++ index2.js
@@ -0,0 +1,10 @@
+const nextISSTimesForMyLocation = require('./iss_promised');
+const printPassTimes = require('./printTime');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
